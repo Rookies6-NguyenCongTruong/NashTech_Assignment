9881ed5281f2995cb718980f63f23d442fe306c5
NguyenCongTruong <congtruongking@gmail.com>
2022-10-22 11:02:05 +0700

    EntityFramework_Core #1 third commit
    removing "public" from Interfaces

--- EFCore/Assignment1/Repositories/Interfaces/IBaseRepository.cs
+++ EFCore/Assignment1/Repositories/Interfaces/IBaseRepository.cs
@@ -5,10 +5,10 @@ namespace Assignment1.Repositories;
 
 public interface IBaseRepository<T> where T : BaseEntity<int>
 {
-    public IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate);
-    public T? Get(Expression<Func<T, bool>> predicate);
-    public T Create(T entity);
-    public T Update(T entity);
-    public bool Delete(T entity);
-    public int SaveChanges();
+    IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate);
+    T? Get(Expression<Func<T, bool>> predicate);
+    T Create(T entity);
+    T Update(T entity);
+    bool Delete(T entity);
+    int SaveChanges();
 }
--- EFCore/Assignment1/Services/Interfaces/IStudentService.cs
+++ EFCore/Assignment1/Services/Interfaces/IStudentService.cs
@@ -4,9 +4,9 @@ namespace Assignment1.Services;
 
 public interface IStudentService
 {
-    public AddStudentResponse Create(AddStudentRequest createModel);
-    public IEnumerable<GetStudentResponse> GetAll();
-    public GetStudentResponse? GetById(int id);
-    public UpdateStudentResponse? Update(int id, UpdateStudentRequest updateModel);
-    public bool Delete(int id);
+    AddStudentResponse Create(AddStudentRequest createModel);
+    IEnumerable<GetStudentResponse> GetAll();
+    GetStudentResponse? GetById(int id);
+    UpdateStudentResponse? Update(int id, UpdateStudentRequest updateModel);
+    bool Delete(int id);
 }
